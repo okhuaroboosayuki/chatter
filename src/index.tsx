3e36a8ffda0b64bfae65542c4de3f373a95af981
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-06-23 03:30:23 +0100

    imported HelmetProvider; & wrapped RouterProvider around it

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./styles/css/style.css";
 import { router } from "./routes/Routes";
+import { HelmetProvider } from "react-helmet-async";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
@@ -10,7 +11,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HelmetProvider>
+      <RouterProvider router={router} />
+    </HelmetProvider>
   </React.StrictMode>
 );
 
