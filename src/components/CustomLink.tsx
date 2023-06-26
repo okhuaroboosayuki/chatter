4eca0c5bf4c719fed5d9e1e9691eb5d7476d5be3
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-06-26 01:44:27 +0100

    created reuseable custom link component

---
+++ src/components/CustomLink.tsx
@@ -0,0 +1,16 @@
+import { NavLink, useResolvedPath, useMatch } from "react-router-dom";
+import "../styles/scss/custom-link.scss";
+
+type CustomLinkProps = {
+  to: string;
+  children: React.ReactNode;
+};
+
+export function CustomLink(props: CustomLinkProps) {
+  const resolvedPath = useResolvedPath(props.to);
+  const isActive = useMatch({ path: resolvedPath.pathname });
+
+  return (
+      <NavLink to={props.to} className={isActive ? "active" : "inactive"}>{props.children}</NavLink>
+  );
+}
