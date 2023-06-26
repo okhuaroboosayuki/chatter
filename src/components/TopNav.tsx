bf4c7805894fedeb51ce21823b3aad49747465ad
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-06-26 16:35:01 +0100

    created feed page, imported important components, and added styles

---
+++ src/components/TopNav.tsx
@@ -0,0 +1,11 @@
+import "../styles/scss/top-nav.scss";
+
+export const TopNav = () => {
+    return (
+        <nav className="top_nav">
+            <div className="search">fgdf</div>
+            <div className="notifications">ddfd</div>
+            <div className="profile">dffdf</div>
+        </nav>
+    )
+}
