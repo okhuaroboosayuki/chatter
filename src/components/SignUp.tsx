a2b9db30b2f20493aeeac6a2fcac5b22e91e3140
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-06-27 18:29:38 +0100

    added a submit fn that handles signup functionalities

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -2,8 +2,40 @@ import "../styles/scss/sign-in.scss";
 import GoogleIcon from "../icons/GoogleIcon";
 import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
 import { Helmet } from "react-helmet-async";
+import { useContext, useRef } from "react";
+import { AuthContext } from "../context/AuthenticationContext";
+import { auth } from "../lib/firebase";
+import { useNavigate } from "react-router-dom";
 
 export const SignUp = () => {
+  const navigate = useNavigate();
+  const { signup } = useContext(AuthContext);
+
+  const firstNameRef = useRef<HTMLInputElement>(null!);
+  const lastNameRef = useRef<HTMLInputElement>(null!);
+  const optionsRef = useRef<HTMLSelectElement>(null!);
+  const emailRef = useRef<HTMLInputElement>(null!);
+  const passwordRef = useRef<HTMLInputElement>(null!);
+  const confirmPasswordRef = useRef<HTMLInputElement>(null!);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const email = emailRef.current?.value;
+      const password = passwordRef.current?.value;
+
+      await signup({
+        auth,
+        email,
+        password,
+      });
+      navigate("/signup/confirm");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <Helmet>
@@ -13,11 +45,12 @@ export const SignUp = () => {
       </Helmet>
       <h2>Register as a Writer/Reader</h2>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="names">
           <div className="input_wrapper">
             <label htmlFor="firstName">First Name</label>
             <input
+              ref={firstNameRef}
               type="text"
               name="firstName"
               id="firstName"
@@ -28,6 +61,7 @@ export const SignUp = () => {
           <div className="input_wrapper">
             <label htmlFor="lastName">Last Name</label>
             <input
+              ref={lastNameRef}
               type="text"
               name="lastName"
               id="lastName"
@@ -39,7 +73,12 @@ export const SignUp = () => {
 
         <div className="profile_details_options input_wrapper">
           <label htmlFor="options">You are joining as?</label>
-          <select name="options" id="options" className="options">
+          <select
+            name="options"
+            id="options"
+            className="options"
+            ref={optionsRef}
+          >
             <option value="writer">Writer</option>
             <option value="reader">Reader</option>
             <option value="reader">Writer & Reader</option>
@@ -49,6 +88,7 @@ export const SignUp = () => {
         <div className="input_wrapper">
           <label htmlFor="email">Email Address</label>
           <input
+            ref={emailRef}
             type="email"
             name="email"
             id="email"
@@ -61,6 +101,7 @@ export const SignUp = () => {
           <label htmlFor="password">Password</label>
           <div className="input">
             <input
+              ref={passwordRef}
               type="password"
               name="password"
               id="password"
@@ -74,6 +115,7 @@ export const SignUp = () => {
           <label htmlFor="confirmPassword">Confirm password</label>
           <div className="input">
             <input
+              ref={confirmPasswordRef}
               type="password"
               name="confirmPassword"
               id="confirmPassword"
