41c3e4b1d3e0ab518eba9ce43e3440c094a052d6
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-06-27 18:28:07 +0100

    wrapped SignIn component with Context API

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -3,6 +3,7 @@ import "../styles/scss/sign-in.scss";
 import { SignInHeader } from "../components/SignInHeader";
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import BackIcon from "../icons/BackIcon";
+import { AuthContextProvider } from "../context/AuthenticationContext";
 
 export const SignIn = () => {
   const navigate = useNavigate();
@@ -26,55 +27,57 @@ export const SignIn = () => {
   // }, []);
 
   return (
-    <section className="register">
-      <div className="register_container">
-        <div
-          className="left"
-          // ref={blurredImageDivRef}
-        >
-          <img
-            // ref={imgRef}
-            src={require("../images/lady-writing-on-a-book.png")}
-            alt="writer"
-            loading="lazy"
-          />
-          <div className="left_content-container">
-            <div className="left_content">
-              <h2>chatter</h2>
-              <p>
-                Unleash the Power of Words, Connect with Like-minded Readers and
-                Writers
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="right">
-          {location.pathname === "/signup/confirm" ? (
-            <div className="back_icon">
-              <BackIcon className="icon" onClick={() => navigate(-1)} />
-              <span>Back</span>
-            </div>
-          ) : (
-            ""
-          )}
-          <div className="right_container">
-            {location.pathname === "/signup/confirm" ? (
-              ""
-            ) : (
-              <div className="links">
-                <SignInHeader to={"/signup"} className="link_one">
-                  Register
-                </SignInHeader>
-                <SignInHeader to={"login"} className="link_two">
-                  Log In
-                </SignInHeader>
+    <AuthContextProvider>
+      <section className="register">
+        <div className="register_container">
+          <div
+            className="left"
+            // ref={blurredImageDivRef}
+          >
+            <img
+              // ref={imgRef}
+              src={require("../images/lady-writing-on-a-book.png")}
+              alt="writer"
+              loading="lazy"
+            />
+            <div className="left_content-container">
+              <div className="left_content">
+                <h2>chatter</h2>
+                <p>
+                  Unleash the Power of Words, Connect with Like-minded Readers
+                  and Writers
+                </p>
               </div>
+            </div>
+          </div>
+
+          <div className="right">
+            {location.pathname === "/signup/confirm" ? (
+              <div className="back_icon">
+                <BackIcon className="icon" onClick={() => navigate(-1)} />
+                <span>Back</span>
+              </div>
+            ) : (
+              ""
             )}
-            <Outlet />
+            <div className="right_container">
+              {location.pathname === "/signup/confirm" ? (
+                ""
+              ) : (
+                <div className="links">
+                  <SignInHeader to={"/signup"} className="link_one">
+                    Register
+                  </SignInHeader>
+                  <SignInHeader to={"login"} className="link_two">
+                    Log In
+                  </SignInHeader>
+                </div>
+              )}
+              <Outlet />
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </AuthContextProvider>
   );
 };
