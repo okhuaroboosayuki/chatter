50db0561aa40948d3134fa93c8a0025cf397a764
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-06-27 18:25:47 +0100

    created a types folder for holding strict type declarations

---
+++ src/types/Types.tsx
@@ -0,0 +1,15 @@
+export type AuthUser = {
+    id?: string;
+    firstName?: string;
+    lastName?: string;
+    options?: string;
+    email?: string;
+    password?: string;
+    confirmPassword?: string;
+}
+
+export type SignupProps = {
+    auth: any;
+    email: string;
+    password: string;
+}
