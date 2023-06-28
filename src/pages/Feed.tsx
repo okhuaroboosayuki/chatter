34cfbed07b05b5a8786b1d03f63cfdd4f6d66fb8
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-06-28 23:58:58 +0100

    wrapped feed section in context provider

--- src/pages/Feed.tsx
+++ src/pages/Feed.tsx
@@ -2,11 +2,9 @@ import { Helmet } from "react-helmet-async";
 import { SideNav } from "../components/SideNav";
 import { TopNav } from "../components/TopNav";
 import "../styles/scss/feed.scss";
-// import { AuthContext } from "../context/AuthenticationContext";
-// import { useContext } from "react";
+import { AuthContextProvider } from "../context/AuthenticationContext";
 
 export const Feed = () => {
-  
   return (
     <>
       <Helmet>
@@ -14,27 +12,29 @@ export const Feed = () => {
         <meta name="description" content="Register to get access to the app" />
         <link rel="canonical" href="/signup" />
       </Helmet>
-      <section className="feed">
-        <SideNav />
-        <div className="feed_container">
-          <TopNav />
+      <AuthContextProvider>
+        <section className="feed">
+          <SideNav />
+          <div className="feed_container">
+            <TopNav />
 
-          <div className="feed_posts_container">
-            <div className="feed_heading">
-              <div className="feed_tir">
-                {/* <h2></h2> */}
-                <p></p>
+            <div className="feed_posts_container">
+              <div className="feed_heading">
+                <div className="feed_title">
+                  {/* <h2></h2> */}
+                  <p></p>
+                </div>
+                <div className="new_post_btn"></div>
               </div>
-              <div className="new_post_btn"></div>
-            </div>
 
-            <div className="feed_categories">
-              {/* <Link to={#ggk}></Link> */}
+              <div className="feed_categories">
+                {/* <Link to={#ggk}></Link> */}
+              </div>
+              <div className="feed_posts"></div>
             </div>
-            <div className="feed_posts"></div>
           </div>
-        </div>
-      </section>
+        </section>
+      </AuthContextProvider>
     </>
   );
 };
