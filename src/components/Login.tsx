30b30b3e71f5a487aeb0bf68a999047363aa16b7
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-06-28 23:53:51 +0100

    used formik and yup for form validation

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,40 +1,42 @@
 import { Helmet } from "react-helmet-async";
 import "../styles/scss/sign-in.scss";
-import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
+import VisibilityOffOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthenticationContext";
-import { useContext, useRef } from "react";
-import { auth } from '../lib/Firebase';
+import { useContext } from "react";
+import { auth } from "../lib/Firebase";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 export const Login = () => {
   const navigate = useNavigate();
 
   const { login } = useContext(AuthContext);
 
-  const emailRef = useRef<HTMLInputElement>(null!);
-  const passwordRef = useRef<HTMLInputElement>(null!);
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: async (values) => {
+      try {
+        const email = values.email;
+        const password = values.password;
+        
+        await login({auth, email, password});
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+        const loggedInUserId = auth.currentUser?.uid;
 
-    try {
-      const email = emailRef.current?.value;
-      const password = passwordRef.current?.value;
-
-      await login({
-        auth,
-        email,
-        password,
-      });
-
-      console.log(auth.currentUser?.uid);
-      const loggedInUserId = auth.currentUser?.uid;
-      
-      navigate(`/${loggedInUserId}/feed`);
-    } catch (error) {
-      console.log(error);
-    }
-  }
+        navigate(`/${loggedInUserId}/feed`);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email("Invalid email address").required("An email address is required"),
+      password: Yup.string().required("Password is required"),
+    }),
+  });
 
   return (
     <>
@@ -46,32 +48,42 @@ export const Login = () => {
       <div className="login_container">
         <h2 className="login_header">Welcome Back</h2>
 
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={formik.handleSubmit}>
           <div className="input_wrapper">
             <label htmlFor="email">Email Address</label>
             <input
-            ref={emailRef}
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
               type="email"
               name="email"
               id="email"
               className="email_input"
               placeholder="Johndoe@gmail.com"
             />
+            {formik.errors.email && formik.touched.email && (
+              <div className="error">{formik.errors.email}</div>
+            )}
           </div>
 
           <div className="input_wrapper">
             <label htmlFor="password">Password</label>
             <div className="input">
               <input
-              ref={passwordRef}
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
                 type="password"
                 name="password"
                 id="password"
                 placeholder="**********"
                 className="password_input"
               />
-              <VisibilityOutlinedIcon className="visibility_icon" />
+              <VisibilityOffOutlinedIcon className="visibility_icon" />
             </div>
+            {formik.errors.password && formik.touched.password && (
+              <div className="error">{formik.errors.password}</div>
+            )}
           </div>
 
           <button type="submit">Log in</button>
