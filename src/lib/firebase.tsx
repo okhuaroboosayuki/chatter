e7c04472e9c36fb141f7a4c24124a003eb9d22c7
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-06-28 01:47:47 +0100

    added google auth provider instance

--- src/lib/firebase.tsx
+++ src/lib/firebase.tsx
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // import { getAnalytics } from "firebase/analytics";
 
 
@@ -19,4 +19,8 @@ const app = initializeApp(firebaseConfig);
 
 // const analytics = getAnalytics(app);
 
+// authentication instance
 export const auth = getAuth(app);
+
+// google auth instance
+export const provider = new GoogleAuthProvider();
