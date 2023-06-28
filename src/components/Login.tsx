831f3bb384c4f1fcb19de3f3b28211ab208168ed
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-06-28 01:46:33 +0100

    added login with email and password auth functionalities

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,8 +1,41 @@
 import { Helmet } from "react-helmet-async";
 import "../styles/scss/sign-in.scss";
 import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthenticationContext";
+import { useContext, useRef } from "react";
+import { auth } from '../lib/Firebase';
 
 export const Login = () => {
+  const navigate = useNavigate();
+
+  const { login } = useContext(AuthContext);
+
+  const emailRef = useRef<HTMLInputElement>(null!);
+  const passwordRef = useRef<HTMLInputElement>(null!);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const email = emailRef.current?.value;
+      const password = passwordRef.current?.value;
+
+      await login({
+        auth,
+        email,
+        password,
+      });
+
+      console.log(auth.currentUser?.uid);
+      const loggedInUserId = auth.currentUser?.uid;
+      
+      navigate(`/${loggedInUserId}/feed`);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <>
       <Helmet>
@@ -13,10 +46,11 @@ export const Login = () => {
       <div className="login_container">
         <h2 className="login_header">Welcome Back</h2>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="input_wrapper">
             <label htmlFor="email">Email Address</label>
             <input
+            ref={emailRef}
               type="email"
               name="email"
               id="email"
@@ -29,6 +63,7 @@ export const Login = () => {
             <label htmlFor="password">Password</label>
             <div className="input">
               <input
+              ref={passwordRef}
                 type="password"
                 name="password"
                 id="password"
