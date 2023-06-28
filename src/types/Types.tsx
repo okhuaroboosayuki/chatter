646eca0ea434e037cc2c1b228ce7bfd532b6b09a
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-06-28 23:56:47 +0100

    removed AuthUser type & add more key value pairs props for SignUpProps

--- src/types/Types.tsx
+++ src/types/Types.tsx
@@ -1,17 +1,11 @@
-export type AuthUser = {
-    id?: string;
+export type SignupProps = {
+    auth?: any;
+    email: string;
+    password: string;
     firstName?: string;
     lastName?: string;
     options?: string;
-    email?: string;
-    password?: string;
     confirmPassword?: string;
-}
-
-export type SignupProps = {
-    auth: any;
-    email: string;
-    password: string;
 }
 
 export type GoogleSignInProps = {
