1a365717626d06410a5ce4a761530a90bb7ded13
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-06-28 23:55:33 +0100

    used formik and yup for form validation

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,57 +1,66 @@
 import "../styles/scss/sign-in.scss";
 import GoogleIcon from "../icons/GoogleIcon";
-import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
+import VisibilityOffOutlinedIcon from "@mui/icons-material/VisibilityOffOutlined";
 import { Helmet } from "react-helmet-async";
-import { useContext, useRef, useState } from "react";
+import { useContext } from "react";
 import { AuthContext } from "../context/AuthenticationContext";
-import { auth, provider } from '../lib/Firebase';
+import { auth, provider } from "../lib/Firebase";
 import { useNavigate } from "react-router-dom";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 export const SignUp = () => {
   const navigate = useNavigate();
   const { signup, googleSignIn } = useContext(AuthContext);
 
-  const [loading, setLoading] = useState(false);
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      options: "",
+    },
+    onSubmit: async (values) => {
+      try {
+        const email = values.email;
+        const password = values.password;
 
-  const firstNameRef = useRef<HTMLInputElement>(null!);
-  const lastNameRef = useRef<HTMLInputElement>(null!);
-  const optionsRef = useRef<HTMLSelectElement>(null!);
-  const emailRef = useRef<HTMLInputElement>(null!);
-  const passwordRef = useRef<HTMLInputElement>(null!);
-  const confirmPasswordRef = useRef<HTMLInputElement>(null!);
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    try {
-      const email = emailRef.current?.value;
-      const password = passwordRef.current?.value;
-
-      setLoading(true);
-
-      await signup({
-        auth,
-        email,
-        password,
-      });
-      navigate("/signup/login");
-    } catch (error) {
-      console.log(error);
-      setLoading(false);
-    }
-  };
+        await signup({
+          auth,
+          email,
+          password,
+        });
+        navigate("/signup/login");
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    validationSchema: Yup.object({
+      firstName: Yup.string().required("First Name is required"),
+      lastName: Yup.string().required("Last Name is required"),
+      options: Yup.string().required("Select an option"),
+      email: Yup.string().email("Invalid email address").required("An email address is required"),
+      password: Yup.string().required("Password is required"),
+      //check if password and confirm password match
+      confirmPassword: Yup.string()
+        .oneOf([Yup.ref("password")], "Passwords do not match")
+        .required("Confirm Password is required"),
+    }),
+  });
 
   const handleGoogleSignIn = async () => {
     try {
-      setLoading(true);
-      await googleSignIn({auth, provider});
+      await googleSignIn({ auth, provider });
 
-      navigate("/signup/login");
+      const loggedInUserId = auth.currentUser?.uid;
+
+      navigate(`/${loggedInUserId}/feed`);
     } catch (error) {
-      setLoading(false);
       console.log(error);
     }
-  }
+  };
 
   return (
     <>
@@ -60,33 +69,41 @@ export const SignUp = () => {
         <meta name="description" content="Register to get access to the app" />
         <link rel="canonical" href="/signup" />
       </Helmet>
-      <h2>Register as a Writer/Reader
-      {loading && <p>Loading...</p>}
-      </h2>
+      <h2>Register as a Writer/Reader</h2>
 
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={formik.handleSubmit}>
         <div className="names">
           <div className="input_wrapper">
             <label htmlFor="firstName">First Name</label>
             <input
-              ref={firstNameRef}
               type="text"
               name="firstName"
               id="firstName"
               className="first_name_input"
               placeholder="John"
+              value={formik.values.firstName}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
             />
+            {formik.errors.firstName && formik.touched.firstName && (
+              <div className="error">{formik.errors.firstName}</div>
+            )}
           </div>
           <div className="input_wrapper">
             <label htmlFor="lastName">Last Name</label>
             <input
-              ref={lastNameRef}
               type="text"
               name="lastName"
               id="lastName"
               className="last_name_input"
               placeholder="Doe"
+              value={formik.values.lastName}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
             />
+            {formik.errors.lastName && formik.touched.lastName && (
+              <div className="error">{formik.errors.lastName}</div>
+            )}
           </div>
         </div>
 
@@ -96,70 +113,83 @@ export const SignUp = () => {
             name="options"
             id="options"
             className="options"
-            ref={optionsRef}
+            value={formik.values.options}
+            onChange={formik.handleChange}
           >
+            <option value="Select">Select</option>
             <option value="writer">Writer</option>
             <option value="reader">Reader</option>
             <option value="reader">Writer & Reader</option>
           </select>
+          {formik.errors.options && formik.touched.options && (
+            <div className="error">{formik.errors.options}</div>
+          )}
         </div>
 
         <div className="input_wrapper">
           <label htmlFor="email">Email Address</label>
           <input
-            ref={emailRef}
             type="email"
             name="email"
             id="email"
             className="email_input"
             placeholder="Johndoe@gmail.com"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
           />
+          {formik.errors.email && formik.touched.email && (
+            <div className="error">{formik.errors.email}</div>
+          )}
         </div>
 
         <div className="input_wrapper">
           <label htmlFor="password">Password</label>
           <div className="input">
             <input
-              ref={passwordRef}
               type="password"
               name="password"
               id="password"
               placeholder="**********"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
             />
-            <VisibilityOutlinedIcon className="visibility_icon" />
+            <VisibilityOffOutlinedIcon className="visibility_icon" />
           </div>
+          {formik.errors.password && formik.touched.password && (
+            <div className="error">{formik.errors.password}</div>
+          )}
         </div>
 
         <div className="input_wrapper">
           <label htmlFor="confirmPassword">Confirm password</label>
           <div className="input">
             <input
-              ref={confirmPasswordRef}
               type="password"
               name="confirmPassword"
               id="confirmPassword"
               placeholder="**********"
+              value={formik.values.confirmPassword}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
             />
-            <VisibilityOutlinedIcon className="visibility_icon" />
+            <VisibilityOffOutlinedIcon className="visibility_icon" />
           </div>
+          {formik.errors.confirmPassword && formik.touched.confirmPassword && (
+            <div className="error">{formik.errors.confirmPassword}</div>
+          )}
         </div>
 
-        <button type="submit">Create account</button>
+        <button type="submit" disabled={formik.isSubmitting}>
+          Create account
+        </button>
 
         <div className="or_sign_with">
           <div className="google">
             <span onClick={handleGoogleSignIn}>
               <GoogleIcon />
               <div>Sign up with google</div>
-            </span>
-          </div>
-          <div className="linkedIn">
-            <span>
-              <img
-                src={require("../images/linkedIn-icon.png")}
-                alt="LinkedIn Icon"
-              />
-              <div>Sign up with Linkedin</div>
             </span>
           </div>
         </div>
