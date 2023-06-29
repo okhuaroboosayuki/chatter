705c9adbb832892d2846d972d7d87ad620b3fd8e
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-06-29 21:13:21 +0100

    added search, notification, & proflie pic contents to top nav; & added styles to the stylesheet

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -1,11 +1,28 @@
 import "../styles/scss/top-nav.scss";
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import SearchIcon from "../icons/SearchIcon";
 
 export const TopNav = () => {
-    return (
-        <nav className="top_nav">
-            <div className="search">fgdf</div>
-            <div className="notifications">ddfd</div>
-            <div className="profile">dffdf</div>
-        </nav>
-    )
-}
+  return (
+    <nav className="top_nav">
+      <div className="search_wrapper">
+        <div className="search_input">
+          <SearchIcon className="search_icon" />
+          <input type="search" name="search" id="search" className="search" placeholder="search chatter" />
+        </div>
+      </div>
+      <div className="top_nav_right">
+        <div className="notifications">
+          <NotificationsOutlinedIcon className="notifications_icon" />
+          <span className="notifications_count">15</span>
+        </div>
+        <div className="profile_pic">
+          <img
+            src={require("../images/profile-picture.png")}
+            alt="profile pic"
+          />
+        </div>
+      </div>
+    </nav>
+  );
+};
