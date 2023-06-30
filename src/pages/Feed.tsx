6ddb938fbd5d1457d35c614883a3e4e14927f965
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-06-30 01:54:12 +0100

    added proper link to Helmet component, & added contents

--- src/pages/Feed.tsx
+++ src/pages/Feed.tsx
@@ -2,15 +2,25 @@ import { Helmet } from "react-helmet-async";
 import { SideNav } from "../components/SideNav";
 import { TopNav } from "../components/TopNav";
 import "../styles/scss/feed.scss";
-import { AuthContextProvider } from "../context/AuthenticationContext";
+import {
+  AuthContext,
+  AuthContextProvider,
+} from "../context/AuthenticationContext";
+import PenIcon from "../icons/PenIcon";
+import { useContext } from "react";
 
 export const Feed = () => {
+  const { currentUser } = useContext(AuthContext);
+
   return (
     <>
       <Helmet>
         <title>Feed</title>
-        <meta name="description" content="Register to get access to the app" />
-        <link rel="canonical" href="/signup" />
+        <meta
+          name="description"
+          content="Explore different content you’d love"
+        />
+        <link rel="canonical" href={`/feed/${currentUser?.uid}`} />
       </Helmet>
       <AuthContextProvider>
         <section className="feed">
@@ -21,10 +31,13 @@ export const Feed = () => {
             <div className="feed_posts_container">
               <div className="feed_heading">
                 <div className="feed_title">
-                  {/* <h2></h2> */}
-                  <p></p>
+                  <h2>FEED</h2>
+                  <p>Explore different content you’d love </p>
                 </div>
-                <div className="new_post_btn"></div>
+                <div className="new_post_btn">
+                  <PenIcon className="new_post_icon" />
+                  <p>Post new content</p>
+                </div>
               </div>
 
               <div className="feed_categories">
