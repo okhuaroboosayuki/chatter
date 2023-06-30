d813f5dafc68cc326e81b3d324c5fa0cb0cb9d4f
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-06-30 01:44:53 +0100

    created 404 page to catch wrong routes & added styles

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,1 +1,53 @@
-export const ErrorPage = () => {};
+import { useNavigate } from "react-router-dom";
+import SpaceMan from "../components/SpaceMan";
+import "../styles/scss/error-page.scss";
+import { AuthContext } from "../context/AuthenticationContext";
+import { useContext } from "react";
+import { Helmet } from "react-helmet-async";
+
+export const ErrorPage = () => {
+  const navigate = useNavigate();
+
+  const { currentUser } = useContext(AuthContext);
+
+  const handleNavigate = () => {
+    if (currentUser) {
+      navigate(`/feed/${currentUser.uid}`);
+    } else {
+      navigate("/");
+    }
+  };
+
+  return (
+    <>
+      <Helmet>
+        <title>404</title>
+        <meta
+          name="description"
+          content="The page you are looking for does not exist. How you got here is a
+            mystery"
+        />
+        <link rel="canonical" href="*" />
+      </Helmet>
+      <div className="error_page_container">
+        <div className="row">
+          <div className="space_man_container">
+            <SpaceMan className="space_man_icon" height={700} width={700} />
+          </div>
+          <div className="col-md-6 align-self-center error_content">
+            <h1>404</h1>
+            <h2>UH OH! You're lost.</h2>
+            <p>
+              The page you are looking for does not exist. How you got here is a
+              mystery. But you can click the button below to go back to the
+              homepage.
+            </p>
+            <button className="btn" onClick={handleNavigate}>
+              HOME
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
