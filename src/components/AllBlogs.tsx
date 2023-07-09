d25c972d1029341eb4557ab0304c887d6f580c38
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-07-09 01:02:01 +0100

    called all blogs from firestore db

--- src/components/AllBlogs.tsx
+++ src/components/AllBlogs.tsx
@@ -1,73 +1,112 @@
+import {
+  DocumentData,
+  collectionGroup,
+  getDocs,
+  query,
+} from "firebase/firestore";
 import CommentsIcon from "../icons/CommentsIcon";
 import HeartIcon from "../icons/HeartIcon";
 import OpenBookIcon from "../icons/OpenBookIcon";
 import SmallChartIcon from "../icons/SmallChartIcon";
 import "../styles/scss/all-blogs.scss";
+import { db } from "../lib/Firebase";
+import { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../context/AuthenticationContext";
 
 export const AllBlogs = () => {
+  const { currentUser } = useContext(AuthContext);
+
+  const [blogContent, setBlogContent] = useState<DocumentData>([]);
+
+  const fetchAllData = async () => {
+    // get a subcollection from all the users called 'posts'
+    const posts = query(collectionGroup(db, "posts"));
+
+    // get all the documents from the 'posts' collection
+    const querySnapshot = await getDocs(posts);
+
+    // map through the documents and get the data
+    const blogs = querySnapshot.docs
+      .map((doc) => ({ id: doc.id, ...doc.data() }));
+
+    // set blogContent to the array of data
+    setBlogContent(blogs);
+  };
+
+  useEffect(() => {
+    fetchAllData();
+  }, []);
+
   return (
     <section className="all_blogs">
-      <article className="single_blog">
-        <div className="author_details">
-          <div className="left">
-            <img
-              src={require("../images/profile-pic-2.png")}
-              alt="Author"
-              className="author_image"
-            />
-          </div>
+      {blogContent.map((blog: any) => (
+        <article className="single_blog" key={blog.id}>
+          <div className="author_details" key={blog.author}>
+            <div className="left">
+              <img
+                src={
+                  blog.authorImage
+                    ? blog.authorImage
+                    : require("../images/no-profile-pic.webp")
+                }
+                alt={blog.author}
+                className="author_image"
+              />
+            </div>
 
-          <div className="right">
-            <div className="author_name">Grace Ikpang</div>
-            <div className="author_title">
-              <span className="title">Product designer.</span>
-              <span className="date_publish_at">
-                {new Date().toLocaleString("en-US", {
-                  month: "long",
-                  day: "numeric",
-                  year: "numeric",
-                })}
-              </span>
+            <div className="right">
+              <div className="author_name">{blog.author}</div>
+              <div className="author_title">
+                <span className="title">
+                  {blog.authorDesignation ? blog.authorDesignation : "Writer"}.
+                </span>
+                <span className="date_publish_at">{blog.createdAt}</span>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="blog_title">
-          <h3 className="blog_title_heading">
-            Starting out as a Frontend Engineer
-          </h3>
-          <p className="blog_post_reading_time">
-            <OpenBookIcon className="open_book_icon" />
-            <span>10 mins read</span>
-          </p>
-        </div>
-        <div className="blog_content">
-          <p className="blog_content_text">
-            Embarking on a journey as a Frontend Engineer can be an exhilarating
-            and fulfilling experience. As a profession that bridges the realms
-            of art, technology, and problem-solving, product design offers an
-            opportunity to shape the way people interact with the world around
-            them.
-          </p>
-          <div className="blog_content_image">
-            <img src={require("../images/blog-post-pic.png")} alt="post pic" />
+          <div className="blog_title" key={blog.title}>
+            <h3 className="blog_title_heading">
+              <Link to={`/feed/${currentUser?.uid}/blogs/${blog.id}`}>
+                {blog.title}
+              </Link>
+            </h3>
+            <p className="blog_post_reading_time">
+              <OpenBookIcon className="open_book_icon" />
+              <span>{blog.timeToRead}</span>
+            </p>
           </div>
+          <div className="blog_content" key={blog.description}>
+            <p className="blog_content_text">{blog.description}</p>
+            <div className="blog_content_image">
+              <Link to={`/feed/${currentUser?.uid}/blogs/${blog.id}`}>
+                <img src={blog.image} alt="post pic" />
+              </Link>
+            </div>
 
-          <div className="blog_post_analytics">
-            <div className="comments">
-              <CommentsIcon className="comments_icon" />
-              <span className="number_of_comments">256</span>
-            </div>
-            <div className="likes">
-              <HeartIcon className="likes_icon" />
-              <span className="number_of_likes">1.2k</span>
-            </div>
-            <div className="views">
-              <SmallChartIcon className="views_icon" />
-              <span className="number_of_views">6.4k views</span>
+            <div className="blog_post_analytics">
+              <div className="comments">
+                <CommentsIcon className="comments_icon" />
+                <span className="number_of_comments">
+                  {blog.comments > 0 ? blog.comments + "k" : "0"}
+                </span>
+              </div>
+              <div className="likes">
+                <HeartIcon className="likes_icon" />
+                <span className="number_of_likes">
+                  {blog.likes > 0 ? blog.likes + "k" : "0"}
+                </span>
+              </div>
+              <div className="views">
+                <SmallChartIcon className="views_icon" />
+                <span className="number_of_views">
+                  {blog.views > 0 ? blog.views + "k views" : "0"}
+                </span>
+              </div>
             </div>
           </div>
-        </div>
-      </article>
+        </article>
+      ))}
     </section>
   );
 };
