7d8b081ffaa354560aec50db9ff35dedc4bf3bee
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-07-09 01:01:22 +0100

    added a conditional statement for rendering currently signed in user's picture

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -1,14 +1,26 @@
 import "../styles/scss/top-nav.scss";
-import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
 import SearchIcon from "../icons/SearchIcon";
+import { AuthContext } from "../context/AuthenticationContext";
+import { useContext } from "react";
 
 export const TopNav = () => {
+  const { currentUser } = useContext(AuthContext);
+
+  const userImage = currentUser?.photoURL;
+
   return (
     <nav className="top_nav">
       <div className="search_wrapper">
         <div className="search_input">
           <SearchIcon className="search_icon" />
-          <input type="search" name="search" id="search" className="search" placeholder="search chatter" />
+          <input
+            type="search"
+            name="search"
+            id="search"
+            className="search"
+            placeholder="search chatter"
+          />
         </div>
       </div>
       <div className="top_nav_right">
@@ -17,10 +29,7 @@ export const TopNav = () => {
           <span className="notifications_count">15</span>
         </div>
         <div className="profile_pic">
-          <img
-            src={require("../images/profile-picture.png")}
-            alt="profile pic"
-          />
+         <img src={userImage ? userImage : require("../images/no-profile-pic.webp")} alt="user" />
         </div>
       </div>
     </nav>
