97f0399db7585a143b3fdf861831f7fb5d05e91a
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-07-15 16:24:16 +0100

    created loaders for page load & btn submit load with styles

---
+++ src/components/ButtonLoader.tsx
@@ -0,0 +1,9 @@
+import "../styles/scss/loading.scss";
+
+export const ButtonLoader = () => {
+    return (
+        <div className="btn_loader">
+            <div className="loader"></div>
+        </div>
+    )
+}
---
+++ src/components/Loader.tsx
@@ -0,0 +1,10 @@
+import "../styles/scss/loading.scss";
+
+export const Loader = () => {
+    return (
+        <div className="loading">
+            <div className="logo">chatter</div>
+            <div className="loading_bar"></div>
+        </div>
+    )
+}
