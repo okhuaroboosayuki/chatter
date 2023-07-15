e956978fd39e739a538da60668155df587680b30
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-07-15 17:40:49 +0100

    created a loader component to show before nested components load

---
+++ src/components/ComponentLoader.tsx
@@ -0,0 +1,9 @@
+import "../styles/scss/loading.scss";
+
+export const ComponentLoader = () => {
+  return (
+    <div className="component_loader">
+      <div className="cycle"></div>
+    </div>
+  );
+};
