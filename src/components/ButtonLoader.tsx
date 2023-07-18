282f7eb896791ab054b6888936f061df48bab9de
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-07-18 18:20:41 +0100

    deleted unused loader component files

--- src/components/ButtonLoader.tsx
+++
@@ -1,9 +0,0 @@
-import "../styles/scss/loading.scss";
-
-export const ButtonLoader = () => {
-    return (
-        <div className="btn_loader">
-            <div className="loader"></div>
-        </div>
-    )
-}
--- src/components/ComponentLoader.tsx
+++
@@ -1,9 +0,0 @@
-import "../styles/scss/loading.scss";
-
-export const ComponentLoader = () => {
-  return (
-    <div className="component_loader">
-      <div className="cycle"></div>
-    </div>
-  );
-};
