bbd9c0b8730ef1c9b0b509ba59f6ea36c9f2f7d7
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-07-19 17:49:40 +0100

    added loading effect

--- src/pages/SingleBlog.tsx
+++ src/pages/SingleBlog.tsx
@@ -16,15 +16,22 @@ import { useParams } from "react-router-dom";
 import { db } from "../lib/Firebase";
 import { useEffect } from "react";
 import ReactHtmlParser from "react-html-parser";
+import { Loader } from "../components/Loader";
 
 export const SingleBlog = () => {
   const { currentUser } = useContext(AuthContext);
 
   const [singleBlog, setSingleBlog] = useState<DocumentData>([]);
+  const [loading, setLoading] = useState(false);
 
   const params = useParams();
 
   useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false)
+    }, 5000);
+
     const fetchSinglePost = async () => {
       // get a single post from collectionGroup
       const post = collectionGroup(db, "posts");
@@ -47,72 +54,80 @@ export const SingleBlog = () => {
         <meta name="description" content={singleBlog.description} />
         <link rel="canonical" href={`/feed/${currentUser?.uid}/post/`} />
       </Helmet>
-      <AuthContextProvider>
-        <section className="feed_single_blog_post">
-          <SideNav />
-          <div className="single_blog_post_container">
-            <TopNav />
+      {loading ? (
+        <Loader />
+      ) : (
+        <AuthContextProvider>
+          <section className="feed_single_blog_post">
+            <SideNav />
+            <div className="single_blog_post_container">
+              <TopNav />
 
-            <div className="single_blog_post">
-              <div className="author_details">
-                <div className="left">
-                  <img
-                    src={
-                      singleBlog.authorImage
-                        ? singleBlog.authorImage
-                        : require("../images/no-profile-pic.webp")
-                    }
-                    alt={singleBlog.author}
-                    className="author_image"
-                  />
-                </div>
+              <div className="single_blog_post">
+                <div className="author_details">
+                  <div className="left">
+                    <img
+                      src={
+                        singleBlog.authorImage
+                          ? singleBlog.authorImage
+                          : require("../images/no-profile-pic.webp")
+                      }
+                      alt={singleBlog.author}
+                      className="author_image"
+                    />
+                  </div>
 
-                <div className="right">
-                  <div className="author_name">{singleBlog.author}</div>
-                  <div className="author_title">
-                    <span className="title">
-                      {singleBlog.authorDesignation}
-                    </span>
-                    <span className="date_publish_at">
-                      {singleBlog.createdAt}
-                    </span>
+                  <div className="right">
+                    <div className="author_name">{singleBlog.author}</div>
+                    <div className="author_title">
+                      <span className="title">
+                        {singleBlog.authorDesignation}
+                      </span>
+                      <span className="date_publish_at">
+                        {singleBlog.createdAt}
+                      </span>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="blog_post_reading_time">
-                <div className="blog_post_analytics">
-                  <div className="comments">
-                    <CommentsIcon className="comments_icon" />
-                    <span className="number_of_comments">
-                      {singleBlog.comments}
-                    </span>
+                <div className="blog_post_reading_time">
+                  <div className="blog_post_analytics">
+                    <div className="comments">
+                      <CommentsIcon className="comments_icon" />
+                      <span className="number_of_comments">
+                        {singleBlog.comments}
+                      </span>
+                    </div>
+                    <div className="likes">
+                      <HeartIcon className="likes_icon" />
+                      <span className="number_of_likes">
+                        {singleBlog.likes}
+                      </span>
+                    </div>
+                    <div className="views">
+                      <SmallChartIcon className="views_icon" />
+                      <span className="number_of_views">
+                        {singleBlog.views}
+                      </span>
+                    </div>
                   </div>
-                  <div className="likes">
-                    <HeartIcon className="likes_icon" />
-                    <span className="number_of_likes">{singleBlog.likes}</span>
-                  </div>
-                  <div className="views">
-                    <SmallChartIcon className="views_icon" />
-                    <span className="number_of_views">{singleBlog.views}</span>
-                  </div>
+                  <p className="reading_time">
+                    <OpenBookIcon className="open_book_icon" />
+                    <span>{singleBlog.timeToRead}</span>
+                  </p>
                 </div>
-                <p className="reading_time">
-                  <OpenBookIcon className="open_book_icon" />
-                  <span>{singleBlog.timeToRead}</span>
-                </p>
-              </div>
 
-              <div className="blog_title">
-                <h3 className="blog_title_heading">{singleBlog.title}</h3>
-              </div>
+                <div className="blog_title">
+                  <h3 className="blog_title_heading">{singleBlog.title}</h3>
+                </div>
 
-              <div className="single_blog_post_content">
-                <article>{ReactHtmlParser(singleBlog.content)}</article>
+                <div className="single_blog_post_content">
+                  <article>{ReactHtmlParser(singleBlog.content)}</article>
+                </div>
               </div>
             </div>
-          </div>
-        </section>
-      </AuthContextProvider>
+          </section>
+        </AuthContextProvider>
+      )}
     </>
   );
 };
