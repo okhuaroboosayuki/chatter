595d9da83832f30496f434369853512ce2427669
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-07-30 22:20:24 +0100

    edited back icon placement

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -25,47 +25,47 @@ export const SignIn = () => {
   // }, []);
 
   return (
-      <section className="register">
-        <div className="register_container">
-          <div
-            className="left"
-            // ref={blurredImageDivRef}
-          >
-            <img
-              // ref={imgRef}
-              src={require("../images/lady-writing-on-a-book.png")}
-              alt="writer"
-              loading="lazy"
-            />
-            <div className="left_content-container">
-              <div className="left_content">
-                <h2>chatter</h2>
-                <p>
-                  Unleash the Power of Words, Connect with Like-minded Readers
-                  and Writers
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div className="right">
-              <div className="back_icon">
-                <BackIcon className="icon" onClick={() => navigate("/")} />
-                <span>Back</span>
-              </div>
-            <div className="right_container">
-                <div className="links">
-                  <SignInHeader to={"/signup"} className="link_one">
-                    Register
-                  </SignInHeader>
-                  <SignInHeader to={"login"} className="link_two">
-                    Log In
-                  </SignInHeader>
-                </div>
-              <Outlet />
+    <section className="register">
+      <div className="register_container">
+        <div
+          className="left"
+          // ref={blurredImageDivRef}
+        >
+          <img
+            // ref={imgRef}
+            src={require("../images/lady-writing-on-a-book.png")}
+            alt="writer"
+            loading="lazy"
+          />
+          <div className="left_content-container">
+            <div className="left_content">
+              <h2>chatter</h2>
+              <p>
+                Unleash the Power of Words, Connect with Like-minded Readers and
+                Writers
+              </p>
             </div>
           </div>
         </div>
-      </section>
+
+        <div className="right">
+          <div className="right_container">
+            <div className="back_icon">
+              <BackIcon className="icon" onClick={() => navigate("/")} />
+              <span>Back</span>
+            </div>
+            <div className="links">
+              <SignInHeader to={"/signup"} className="link_one">
+                Register
+              </SignInHeader>
+              <SignInHeader to={"login"} className="link_two">
+                Log In
+              </SignInHeader>
+            </div>
+            <Outlet />
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
