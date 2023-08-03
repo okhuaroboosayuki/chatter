0af1e058294e6a313473eb25a5b11f9be270b9d1
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-08-03 01:53:46 +0100

    added custom links props type

--- src/types/Types.tsx
+++ src/types/Types.tsx
@@ -1,14 +1,19 @@
 export type SignupProps = {
-    auth?: any;
-    email: string;
-    password: string;
-    firstName?: string;
-    lastName?: string;
-    options?: string;
-    confirmPassword?: string;
-}
+  auth?: any;
+  email: string;
+  password: string;
+  firstName?: string;
+  lastName?: string;
+  options?: string;
+  confirmPassword?: string;
+};
 
 export type GoogleSignInProps = {
-    auth: any;
-    provider: any;
-}
+  auth: any;
+  provider: any;
+};
+
+export type CustomLinkProps = {
+  to: string;
+  children: React.ReactNode;
+};
