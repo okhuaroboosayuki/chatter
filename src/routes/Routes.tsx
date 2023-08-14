35b75682375fc9b4b79d3df6ef2147d9cfacaffb
okhuarobo osayuki <osayukiraymond@gmail.com>
2023-08-14 23:59:04 +0100

    moved protected routes functionality into types folder, added suspense wrapper to routes where needed

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,14 +1,14 @@
-import { createBrowserRouter, useNavigate } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import { SignUp } from "../components/SignUp";
 import { Login } from "../components/Login";
 import { Feed } from "../pages/Feed";
 import { SignIn } from "../pages/SignIn";
 import { ErrorPage } from "../pages/ErrorPage";
-import { Suspense, lazy, useContext, useEffect } from "react";
+import { Suspense, lazy } from "react";
 import { AllBlogs } from "../components/AllBlogs";
-import { AuthContext } from "../context/AuthenticationContext";
 import { SingleBlog } from "../pages/SingleBlog";
 import { Loader } from "../components/Loader";
+import { ProtectedRouteOne, ProtectedRouteTwo } from "./ProtectedRoutes";
 
 // lazy load some pages
 const App = lazy(() => import("../App"));
@@ -17,34 +17,11 @@ const NewBlogPost = lazy(() =>
     default: NewBlogPost,
   }))
 );
-const ContactUs = lazy(() => import("../pages/ContactUs").then(({ ContactUs }) => ({ default: ContactUs })));
+const ContactUs = lazy(() =>
+  import("../pages/ContactUs").then(({ ContactUs }) => ({ default: ContactUs }))
+);
 
 // set protected routes
-type ProtectedRouteProps = {
-  children: React.ReactNode;
-};
-
-function ProtectedRoute({ children }: ProtectedRouteProps) {
-  const { currentUser } = useContext(AuthContext);
-  const navigate = useNavigate();
-  const route = window.location.pathname;
-
-  // useEffect(() => {
-  //   if (!currentUser && route === '/feed/:userId') {
-  //     navigate("/signup/login");
-  //   } else
-
-  //   if (currentUser && route === "/signup") {
-  //     navigate(`/feed/${currentUser?.uid}`);
-  //   } else return;
-
-  
-  // }, [currentUser, navigate, route]);
-
-  // useEffect(() => {}, [])
-
-  return <>{children}</>;
-}
 
 export const router = createBrowserRouter([
   {
@@ -65,60 +42,58 @@ export const router = createBrowserRouter([
   },
   {
     path: "/signup",
-    element: <SignIn />,
+    element: (
+      <Suspense fallback={<Loader />}>
+        <ProtectedRouteTwo>
+          <SignIn />
+        </ProtectedRouteTwo>
+      </Suspense>
+    ),
     children: [
       {
         index: true,
-        element: (
-          <ProtectedRoute>
-              <SignUp />
-          </ProtectedRoute>
-        ),
+        element: <SignUp />,
       },
       {
         path: "login",
-        element: (
-          <ProtectedRoute>
-              <Login />
-          </ProtectedRoute>
-        ),
+        element: <Login />,
       },
     ],
   },
   {
     path: "/feed/:userId",
     element: (
-      <ProtectedRoute>
-        <Feed />
-      </ProtectedRoute>
+      <Suspense fallback={<Loader />}>
+        <ProtectedRouteOne>
+          <Feed />
+        </ProtectedRouteOne>
+      </Suspense>
     ),
     children: [
       {
         index: true,
-        element: (
-          <ProtectedRoute>
-            <AllBlogs />
-          </ProtectedRoute>
-        ),
+        element: <AllBlogs />,
       },
     ],
   },
   {
     path: "/feed/:userId/new-post",
     element: (
-      <ProtectedRoute>
-        <Suspense fallback={<Loader />}>
+      <Suspense fallback={<Loader />}>
+        <ProtectedRouteOne>
           <NewBlogPost />
-        </Suspense>
-      </ProtectedRoute>
+        </ProtectedRouteOne>
+      </Suspense>
     ),
   },
   {
     path: "/feed/:userId/post/:blogId",
     element: (
-      <ProtectedRoute>
-        <SingleBlog />
-      </ProtectedRoute>
+      <Suspense fallback={<Loader />}>
+        <ProtectedRouteOne>
+          <SingleBlog />
+        </ProtectedRouteOne>
+      </Suspense>
     ),
   },
   {
